fe5178505bff6d45e8da01e1b546b57f2fd474ac
Jatin <patrajatin0@gmail.com>
2025-12-11 00:53:07 +0530

    setup config

--- server.ts
+++ server.ts
@@ -1,9 +1,11 @@
 
 import app from "./src/App.ts"
+import { config } from "./src/config/config.ts";
+
 console.log("hello");
 
 const startServer = () => {
-    const port = process.env.PORT || 3000;
+    const port = config.port || 3000;
     app.listen(port, () => {
         console.log(`Listening on port:${port}`)
     });
